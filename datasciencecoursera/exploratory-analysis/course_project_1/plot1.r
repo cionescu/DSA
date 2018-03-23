ac3cf5dbcc899f93db9c5c97a61eafc70203a713
Catalin Ionescu <catalin.ionescu282@gmail.com>
2018-03-23 10:15:39 +0200

    Improve the read.csv call

--- datasciencecoursera/exploratory-analysis/course_project_1/plot1.r
+++ datasciencecoursera/exploratory-analysis/course_project_1/plot1.r
@@ -1,6 +1,9 @@
 colClasses <- c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
-data <- read.csv("./household_power_consumption.txt", header=TRUE, sep=";", colClasses=colClasses)
+data <- read.csv("./household_power_consumption.txt", header=TRUE, sep=";", colClasses=colClasses, na.strings = c("?"))
 dates <- paste(data$Date, data$Time)
+
+# Alternative 1-liner data$Datetime <- as.POSIXct(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
+
 data$DateTime <- strptime(dates, "%d/%m/%Y %H:%M:%S")
 data[c("Date", "Time")] <- list(NULL)
 
--- datasciencecoursera/exploratory-analysis/course_project_1/plot2.r
+++ datasciencecoursera/exploratory-analysis/course_project_1/plot2.r
@@ -1,5 +1,5 @@
 colClasses <- c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
-data <- read.csv("./household_power_consumption.txt", header=TRUE, sep=";", colClasses=colClasses)
+data <- read.csv("./household_power_consumption.txt", header=TRUE, sep=";", colClasses=colClasses, na.strings = c("?"))
 dates <- paste(data$Date, data$Time)
 data$DateTime <- strptime(dates, "%d/%m/%Y %H:%M:%S")
 data[c("Date", "Time")] <- list(NULL)
