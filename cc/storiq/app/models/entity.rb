93b4c30c45906d4a0a20cde3980dfc460874211a
Catalin Ionescu <catalin.ionescu282@gmail.com>
2018-06-02 23:25:57 +0300

    feat: Add review averages

--- cc/storiq/app/models/entity.rb
+++ cc/storiq/app/models/entity.rb
@@ -26,4 +26,24 @@ class Entity < ApplicationRecord
   validates :entity_type, :name, presence: true
 
   scope :root, -> { where(entity_id: nil) }
+
+  def average
+    return "N/A" if reviews.none?
+    reviews.pluck(:score).reduce(:+).to_f / reviews.count
+  end
+
+  def average_incl_children
+    reviews = reviews_for(self)
+    return "N/A" if reviews.none?
+    reviews.map(&:score).reduce(:+).to_f / reviews.count
+  end
+
+  def reviews_for node
+    reviews = node.reviews.to_a
+    return reviews if node.children.none?
+    node.children.each do |child|
+      reviews << reviews_for(child)
+    end
+    reviews.flatten
+  end
 end
