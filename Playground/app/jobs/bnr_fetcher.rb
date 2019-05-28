8cac182725d3a878df1cc2493f669eac48fa14a4
Catalin Ionescu <catalin.ionescu282@gmail.com>
2019-05-28 11:48:22 +0300

    Fixes

--- Playground/app/jobs/bnr_fetcher.rb
+++ Playground/app/jobs/bnr_fetcher.rb
@@ -8,6 +8,10 @@ class BnrFetcher < ApplicationJob
     while !(data = response.css("DataSet Body Cube[date='#{date.to_s}'] Rate[currency='USD']").first)
       date = date.prev_day
       puts "Trying for #{date.to_s}"
+      if (invoice.invoice_date - date).to_i > 10
+        puts "❌ Gone too far. Breaking"
+        return
+      end
     end
     invoice.update!(exchange_date: date, exchange_rate: data.inner_text.to_f)
   end
@@ -16,7 +20,7 @@ class BnrFetcher < ApplicationJob
 
   def response
     @_response ||= invoice.yield_self do |invoice|
-      "http://www.bnr.ro/files/xml/years/nbrfxrates#{invoice.invoice_date.year}.xml"
+      "https://www.bnr.ro/files/xml/years/nbrfxrates#{invoice.invoice_date.year}.xml"
     end.yield_self{ |uri| Excon.get(uri).body }
       .yield_self{ |body| Nokogiri::XML(body) }
   end
